2e3b424b7c35e9551b6f0d2536161b02b93ad215
Leandro Lillo <llillo@medallia.com>
2025-09-05 12:45:51 -0300

    Added camera toString. Testing glu error info

---
+++ src/breakout/Breakout.cpp
@@ -0,0 +1,119 @@
+#include "../base/BaseDemo.h"
+#include "PhysicsRunner.h"
+#include "GeometryRenderer.h"
+
+class Entity {
+public:
+  virtual void draw(DefaultRenderer &renderer) = 0;
+};
+
+class Background: public Entity {
+  real width;
+  real height;
+  TextureResource *texture;
+
+
+public:
+  Background(unsigned int width, unsigned int height) : texture(texture) {
+    this->width = width;
+    this->height = height;
+  }
+
+  void resize(unsigned int width, unsigned int height) {
+    this->width = width;
+    this->height = height;
+  }
+
+  void setTexture(TextureResource &texture) {
+    this->texture = &texture;
+
+  }
+
+  void draw(DefaultRenderer &renderer) override {
+    renderer.setTexture(texture);
+    renderer.drawBox(matriz_4x4::traslacion(0,  0, 0), vector(width, height, 1));
+  }
+};
+
+class BreakoutRunner: public BaseDemoRunner {
+  PhysicsRunner *physics = null;
+  LightResource light;
+  Background background;
+  Particle *ball;
+
+  GeometryRenderer geometryRenderer;
+public:
+  BreakoutRunner() : background(0, 0), geometryRenderer(defaultRenderer),
+  light(vector(0, 0, 0), vector(0.6f, 0.6f, 0.6f), vector(0.2f, 0.2f, 0.2f), vector(0.1f, 0.1f, 0.1f), 1.0f) {
+
+  }
+  bool initialize() override {
+    if (!BaseDemoRunner::initialize()) {
+      return false;
+    }
+
+    TextureResource *backgroundTexture = (TextureResource *)this->getResourceManager().load("background.png", MimeTypes::TEXTURE);
+    if(backgroundTexture != null) {
+      background.setTexture(*backgroundTexture);
+    }
+
+    physics = (PhysicsRunner*) this->getContainer()->getRequiredRunner(PhysicsRunner::ID);
+    ParticleManager &particleManager = physics->getParticleManager();
+
+    ball = &particleManager.addParticle(std::make_unique<Particle>(std::make_unique<Sphere>(vector(0, 0, 0), 0.5)));
+
+    return true;
+  }
+
+  LoopResult doLoop() override {
+    defaultRenderer.clear();
+    //defaultRenderer.setLight(&light);
+
+    defaultRenderer.setTexture(null);
+    defaultRenderer.drawAxes(matriz_4x4::identidad);
+
+    background.draw(defaultRenderer);
+    geometryRenderer.render(ball->getBoundingVolume());
+
+    defaultRenderer.render(camera);
+    return LoopResult::CONTINUE;
+  }
+
+  virtual void onResize(unsigned int height, unsigned int width) override {
+    //camera.setPerspectiveProjectionFov(45.0, (GLfloat) width / (GLfloat) height, 0.1, 600.0);
+    camera.setOrthographicProjection(height, width, -10, 10);
+    background.resize(width, height);
+  }
+
+  virtual void onMouseWheel(int wheel) override {
+    vector position = camera.getPosition() - vector(0.0, 0.0, std::min(1.0, 0.1 * wheel));
+    position.z = std::max(-10.0f, std::min(10.0f, position.z));
+    camera.setPosition(position);
+    logger->info("Camera [%s]", camera.toString().c_str());
+  }
+
+};
+
+class Breakout: public Playground {
+public:
+  Breakout(const String &resourcesBasePath) :
+      Playground(resourcesBasePath) {
+  }
+  void initializePlayground() override {
+    Playground::initializePlayground();
+    this->addRunner(std::make_unique<OpenGLRunner>());
+    this->addRunner(std::make_unique<AudioRunner>());
+    this->addRunner(std::make_unique<PhysicsRunner>());
+    this->addRunner(std::make_unique<BreakoutRunner>());
+  }
+};
+
+int main(int argc, char **argv) {
+  String repository = Paths::add(Paths::getDirname(argv[0]), "resources"); //assumes executable lies in playground/target folder
+  Breakout playground(repository);
+  playground.withName("Blockout");
+  printf("\n\nRunning playground [%s]\n", playground.toString().c_str());
+  playground.run();
+  printf("done\n");
+  return 0;
+}
